eb1ad6e38b3a276adbba415349c6c86b59117924
Lukas de Ruiter <lukas_kremlin@hotmail.com>
2021-04-07 15:06:37 +0200

    added box shadow and movement

--- x.js
+++ x.js
@@ -1,18 +1,20 @@
 document.getElementById('humanModelsContainerID').addEventListener('mouseenter', function(){
     humanModelsContainerID.style.width = "500px";
     humanModelsContainerID.style.height = "500px";
+    humanModelsContainerID.style.top = "5%";
     humanModelsContainerID.ontransitionend = function() {
     let models = document.getElementsByClassName("humanModels");
     for(let i = 0; i < models.length; i++){
-        models[i].style.display = "flex";
+        models[i].style.display = "block";
     }
     }
   })
   
   
   document.getElementById('humanModelsContainerID').addEventListener('mouseleave', function(){
-    humanModelsContainerID.style.width = "80px";
-    humanModelsContainerID.style.height = "80px";
+    humanModelsContainerID.style.width = "86px";
+    humanModelsContainerID.style.height = "86px";
+    humanModelsContainerID.style.top = "20%";
     let models = document.getElementsByClassName("humanModels");
     for(let i = 0; i < models.length; i++){
         models[i].style.display = "none";
